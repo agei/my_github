e2405f12852ba087476cf5961fe0a475a6d31009
Jonathan Lin <jonathanhc.lin@moxa.com>
2020-07-09 14:35:25 +0800

    updated, working with new docker build

--- my_script/my_pre_mxopenwrt.sh
+++ my_script/my_pre_mxopenwrt.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-# repo init -u ssh://git@git.moxa.com/net/wireless/mxopenwrt/manifest.git -b master -m default.xml
-# repo sync --no-tags -qc
+repo init -u ssh://git@git.moxa.com/net/wireless/mxopenwrt/manifest.git -b master -m default.xml
+repo sync --no-tags -qc
 
 cd openwrt
-# exec bash
-PWD=`pwd`
-echo $PWD
-echo "this is a test"
-makeqsdk='docker run --rm -it --init -w=$(pwd) -v $(pwd)/../:$(pwd)/../:cached cross-build/qsdk'
-echo $makeqsdk
-$makeqsdk package/symlinks
+make-qsdk package/symlinks
 ln -s ../profiles/awk3252a-premium.config .config
-$makeqsdk defconfig
+make-qsdk defconfig
 
-time $makeqsdk -j8 V=s
+time make-qsdk -j8 V=s
